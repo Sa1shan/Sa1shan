8da7c75fae5d8497f5b972cde82d01d9a00d2727
Sa1shan <154024603+Sa1shan@users.noreply.github.com>
2026-07-13 04:52:36 +0300

    fixed bugs

--- Sa1shanWebSite/obj/Debug/net8.0/Sa1shanWebSite.AssemblyInfo.cs
+++ Sa1shanWebSite/obj/Debug/net8.0/Sa1shanWebSite.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Sa1shanWebSite")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+af85cd66aa1fd8ba23f4ef7e1b9a21f3ba9a8826")]
 [assembly: System.Reflection.AssemblyProductAttribute("Sa1shanWebSite")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Sa1shanWebSite")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
